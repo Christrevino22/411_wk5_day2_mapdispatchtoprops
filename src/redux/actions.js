84a3ddef4f9ef9561a08dae1a71c660291936c7e
Christopher Trevino <chrisdadef@gmail.com>
2021-02-27 16:36:41 -0600

    finally finished yuh

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,6 +1,19 @@
-const addCar = (car) => {
+export const addCar = (car) => {
   return {
     type: "ADD_CAR",
     value: car,
   };
-}
+};
+//WASN'T EXPORTING THE DAMN FUNCTION SO THATS WHY IT WASN'T WORKING!!!!!!! WTF!!!!!
+// GETTING A PRETTY GOOD UNDERSTANDING
+
+//NOW I NEED TO CREATE A REMOVE CAR FUNCTION
+// basically do the same as the add car function
+//THE PRE WORK HAS A MAJORITY OF THE OF THE ANSWERS FOR THE HM
+
+export const removeCar = (index) => {
+  return {
+    type: "REMOVE_CAR",
+    value: index,
+  };
+};
